fbee0c4d8800fb89afd3d1bbdcafc90ebe675cd0
natalia <nata774e@stud.kea.dk>
2023-04-22 20:35:14 +0200

    buyers card first setup

--- src/components/Cards/BuyerCard.js
+++ src/components/Cards/BuyerCard.js
@@ -2,19 +2,16 @@ import styles from "./Card.module.css";
 import icon from "@/assets/icon-input-close.svg";
 import Image from "next/image";
 
-export default function DashboardCard() {
+export default function BuyerCard() {
   return (
     <article className={styles.card}>
       <ul>
+        <li>Id</li>
+        <li>Estate</li>
         <li>Date</li>
-        <li>Name</li>
-        <li>Email</li>
-        <li>Phone</li>
-        <li>Contact</li>
-        <li>Price</li>
-        <li>Zipcode</li>
-        <li>Size</li>
-        <li>Estate type</li>
+        <li>Adults</li>
+        <li>Children</li>
+        <li>Description</li>
       </ul>
       <button className={styles.closeIcon}>
         <Image src={icon.src} width={30} height={30} alt="close" priority />
