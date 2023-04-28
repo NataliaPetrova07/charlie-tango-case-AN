04c1d9d83e68f52cdec2251a0409ea87c77d8846
anna <annabarellaalonso@gmail.com>
2023-04-28 22:11:21 +0200

    A - find a buyer form Material UI

--- src/components/Forms/Form.js
+++ src/components/Forms/Form.js
@@ -1,27 +1,136 @@
+import styles from "../../pages/Home.module.css";
+import { estateTypes } from "@/data/estateTypes";
 
-import TextField from "@mui/material";
-import Button from "@mui/material";
-import { useForm } from "react-hook-form";
+import {
+    TextField,
+    Button,
+    InputAdornment,
+    InputLabel,
+    MenuItem,
+    FormControl,
+    Select
+} from '@mui/material';
 
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 export default function Form() {
 
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
+    let estateType = estateTypes
 
-    console.log(watch("example"));
+    const [price, setPrice] = useState("");
+    const [size, setSize] = useState("");
+    const [zip, setZip] = useState("");
+    const [type, setType] = useState("");
+
+    const [priceError, setPriceError] = useState(false);
+    const [sizeError, setSizeError] = useState(false);
+    const [zipError, setZipError] = useState(false);
+
+    const router = useRouter();
+
+    const handleChange = (e) => {
+        setType(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        //set error state to falso so it won't display
+        setPriceError(false)
+        setSizeError(false)
+        setZipError(false)
+
+        //if the inpt is empty display error
+        if (price == '') {
+            setPriceError(true)
+        }
+        if (size == '') {
+            setSizeError(true)
+        }
+        if (zip == '') {
+            setZipError(true)
+        }
+
+        //if all the fields, on submit send to buyers page
+        if (price && size && zip) {
+            router.push('/buyers');
+        }
+    }
+
+
     return (
-        /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-        <form onSubmit={handleSubmit(onSubmit)}>
-            {/* register your input into the hook by invoking the "register" function */}
-            <input defaultValue="test" {...register("example")} />
 
-            {/* include validation with required or other standard HTML validation rules */}
-            <input {...register("exampleRequired", { required: true })} />
-            {/* errors will return when field validation fails  */}
-            {errors.exampleRequired && <span>This field is required</span>}
+        <form className={styles.form} onSubmit={handleSubmit}>
+            <TextField
+                onChange={(e) => setPrice(e.target.value)}
+                id="price"
+                name="price"
+                label="Price"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                error={priceError}
+                helperText="Incorrect entry."
+            />
+            <TextField
+                onChange={(e) => setSize(e.target.value)}
+                id="size"
+                name="size"
+                label="Property Size"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                //The icon for square meters is not showing, why?
+                endAdornment={<InputAdornment position="end">kg</InputAdornment>}
+                error={sizeError}
+            />
+            <TextField
+                onChange={(e) => setZip(e.target.value)}
+                id="zip"
+                name="zip"
+                label="Zip Code"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                error={zipError}
+            />
 
-            <input type="submit" />
+            <FormControl fullWidth
+                margin="normal">
+                <InputLabel id="type">Property Type</InputLabel>
+                <Select
+                    labelId="type"
+                    id="type"
+                    value={type}
+                    label="Property Type"
+                    onChange={handleChange}
+                >
+                    {estateType.map((estate) => (
+                        <MenuItem value={estate.id} key={estate.id} >{estate.name}</MenuItem>
+                    ))
+                    };
+                </Select>
+            </FormControl>
+            <Button
+                variant="contained"
+                /* onClick={() => console.log("clicked")} */
+                type="submit"
+            >
+                Submit
+            </Button>
         </form>
     );
 }
+
+
+//POPULATE DROP DOWN
+/* https://antdp425.medium.com/populate-dropdown-options-in-react-ad2a54b64f8e */
+
+
+{/* <label for="type" className={styles.label}>Property type</label>
+<select className={styles.option} name="type" id="type" >
+  <option value="Select a estate"> -- Estate Type -- </option>
+  {estateType.map((estate) => (
+    <option value={estate.id} key={estate.id} >{estate.name}</option>
+  ))
+  } */}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import styles from "./Home.module.css";
-/* import { Button } from "@/components/Button"; */
 import Field from "@/components/Forms/Field";
 import { estateTypes } from "@/data/estateTypes";
 import Form from "@/components/Forms/Form";
@@ -10,8 +9,6 @@ import Form from "@/components/Forms/Form";
 
 export default function Home() {
 
-  //POPULATE DROP DOWN
-  /* https://antdp425.medium.com/populate-dropdown-options-in-react-ad2a54b64f8e */
   let estateType = estateTypes
 
   return (
@@ -47,45 +44,40 @@ export default function Home() {
           <form action="/buyers" method="GET" className={styles.form}>
             <label>
               <span className={styles.label}>Zip Code</span>
-              <input name="zipCode" required />
+              <input name="zipCode" />
             </label>
-            <label>
-              <span className={styles.label}>Zip Code</span>
-              <input name="zipCode" required />
-            </label>
-            <button className={styles.button}>Submit</button>
+            {/*  <button className={styles.button}>Submit</button> */}
           </form>
         </div>
-        <div className={styles.content}>
+        {/*  <div className={styles.content}>
           <h2>Find a buyer for you property</h2>
           <form action="/buyers" method="GET" className={styles.form}>
             <Field
               label="Price"
               id="price"
               name="price"
-              required />
+            />
             <Field
               label="Size"
               id="size" name="size"
-              required />
+            />
             <Field
               label="Zip Code"
               hint="hint-code"
               texthint="Must contain four digits"
               id="zip"
               name="zip"
-              required />
+            />
             <label for="type" className={styles.label}>Property type</label>
-            <select className={styles.option} name="type" id="type" required>
+            <select className={styles.option} name="type" id="type" >
               <option value="Select a estate"> -- Estate Type -- </option>
               {estateType.map((estate) => (
                 <option value={estate.id} key={estate.id} >{estate.name}</option>
               ))
               }
             </select>
-            {/* <Button text={"Find buyers"}></Button> */}
           </form>
-        </div>
+        </div> */}
         <div className={styles.content}>
           <h2>Find a buyer for you property - Material UI</h2>
           <Form />
