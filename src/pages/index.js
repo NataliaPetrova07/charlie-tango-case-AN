07622ad5f4e9a383dc4ac8b2ad99fda6d21b5dc4
anna <annabarellaalonso@gmail.com>
2023-05-03 21:58:41 +0200

    A-cleaning data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,13 @@
 import Head from "next/head";
 import styles from "./Home.module.css";
 import Field from "@/components/Forms/Field";
-import { estateTypes } from "@/data/estateTypes";
-import Form from "@/components/Forms/FormSelect";
+
 import FormSelect from "@/components/Forms/FormSelect";
 
 
 
 
 export default function Home() {
-
-  let estateType = estateTypes
 
   return (
     <>
