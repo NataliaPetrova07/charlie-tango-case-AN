cbc759c58a03a1ec4aecf99f9f13295880e0a0ab
anna <annabarellaalonso@gmail.com>
2023-05-03 22:28:52 +0200

    A-clean code + possible fetching

--- src/pages/buyers/index.js
+++ src/pages/buyers/index.js
@@ -12,41 +12,7 @@ export default function Buyers(props) {
         <title>Find buyer | EDC</title>
       </Head>
       <div className="wrapper">
-        <h1 className={styles.headline}>Potential buyers</h1>
-        <p>
-          On this page you get the <code>`query`</code> params like{" "}
-          <code>`zipCode`</code>, and can use them to fetch a list of buyers
-          from the API.
-        </p>
-        <p>
-          Make sure to read the docs on how to fetch data on a page - There are
-          multiple ways of doing it, and you should choose the one that fits
-          your solution best.
-        </p>
-        <ul>
-          <li>
-            <a
-              href="https://nextjs.org/docs/basic-features/data-fetching/get-server-side-props"
-              target="_blank"
-            >
-              next.js - Data fetching
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://react.dev/learn/synchronizing-with-effects#fetching-data"
-              target="_blank"
-            >
-              react.dev - Fetching data
-            </a>
-          </li>
-        </ul>
-        <div className={styles.content}>
-          <h2>Query params:</h2>
-          <pre>
-            <code>{JSON.stringify(query, null, 2)}</code>
-          </pre>
-        </div>
+        <h1>Potential buyers</h1>
         <section className={styles.container}>
           {props.buyers.map((buyer) => (
             <BuyerCard key={buyer.id} {...buyer} />
@@ -77,3 +43,28 @@ export async function getServerSideProps() {
     },
   };
 }
+
+
+
+//   const api = "http://localhost:3000/api/find-buyers?price=${mySeller.price}&size=${mySeller.size}&zipCode=${mySeller.zipCode}";
+
+/* export async function getServerSideProps() {
+  const mySeller = useContext(SellerContext);
+  // const buyer = context.params.buyer;
+  const api = "http://localhost:3000/api/find-buyers?price=" + mySeller.price;
+  const res = await fetch(api);
+  // If no data - no page (404)
+  if (res.status != 200) {
+    return {
+      notFound: true,
+    };
+  }
+  const data = await res.json();
+
+  return {
+    props: {
+      buyers: data,
+    },
+  };
+}
+ */
